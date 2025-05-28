043c13812fca48fb35ba96d980c05290368f5c60
Gleb <143960355+Gleb-a-p@users.noreply.github.com>
2025-05-28 13:59:11 +0300

    Update dialogue.py
    Some minor changes

--- app/modules/core/logic/dialogue.py
+++ app/modules/core/logic/dialogue.py
@@ -9,21 +9,24 @@ import random
 import os
 
 import app.modules.core.logic.config as config
-import app.modules.audio.audio_detection as a_d
-import app.modules.audio.audio_speaking as a_s
+import app.modules.audio.audio_detection as audio_detection
+import app.modules.audio.audio_speaking as audio_speaking
 from app.modules.core.state_interface import StateInterface
 
 
-# voice command reading function
+# Voice command reading function
 def va_respond(message: str, client, dialogue_history, mod) -> None:
     print(message)
-    detected_message = a_d.va_wake_word_detection(message)
+
+    detected_message = audio_detection.va_wake_word_detection(message)
     if detected_message:
         if message:
             print(message)
+
         print(f"К ассистенту обращаются с запросом [{detected_message}]")
         cmd = recognize_cmd(filter_cmd(message))
         print(cmd)
+
         if (
                 cmd["cmd"] not in config.VA_SPEAKING_CMD_LIST.keys() or
                 cmd["percent"] < config.CMD_PERCENT_DETECTION
@@ -33,38 +36,47 @@ def va_respond(message: str, client, dialogue_history, mod) -> None:
         ):
             if mod == "base":
                 try:
-                    a_s.va_speak(generate_response(
-                        dialogue_history,
-                        message,
-                        mod,
-                        client
-                    ))
+                    audio_speaking.va_speak(
+                        generate_response(
+                            dialogue_history,
+                            message,
+                            mod,
+                            client
+                        )
+                    )
+
                 except Exception as err:
                     print(err)
             elif mod == "free":
                 try:
-                    a_s.va_speak(generate_response(
-                        dialogue_history=dialogue_history,
-                        message=message,
-                        mod=mod
-                    ))
+                    audio_speaking.va_speak(
+                        generate_response(
+                            dialogue_history=dialogue_history,
+                            message=message,
+                            mod=mod
+                        )
+                    )
+
                 except Exception as err:
                     print(err)
         else:
             execute_cmd(cmd['cmd'])
 
 
-# voice command correction function
+# Voice command correction function
 def filter_cmd(raw_voice: str) -> str:
     cmd = raw_voice
+
     for word in config.VA_WAKE_WORD_LIST:
         cmd = cmd.replace(word, '').strip()
+
     for word in config.VA_TBR:
         cmd = cmd.replace(word, '').strip()
+
     return cmd
 
 
-# voice command recognition function
+# Voice command recognition function
 def recognize_cmd(cmd: str) -> dict[str, str | int]:
     rc = {"cmd": '', "percent": 0}
     for command, variants in config.VA_SPEAKING_CMD_LIST.items():
@@ -73,29 +85,36 @@ def recognize_cmd(cmd: str) -> dict[str, str | int]:
             if vrt > rc["percent"]:
                 rc["cmd"] = command
                 rc["percent"] = vrt
+
     for command, variants in config.VA_VOID_CMD_LIST.items():
         for word in variants:
             vrt = fuzz.ratio(cmd, word)
             if vrt > rc["percent"]:
                 rc["cmd"] = command
                 rc["percent"] = vrt
+
     return rc
 
 
-# voice command execution function
+# Voice command execution function
 def execute_cmd(cmd: str) -> None:
     state_interface: StateInterface = StateInterface()
     text = config.NOT_UNDERSTAND_ANSWER
+
     if cmd in config.VA_SPEAKING_CMD_LIST:
         match cmd:
             case "help":
                 text = config.GREETING_MESSAGE
+
             case "joke":
                 text = random.choice(config.JOKER_LIST)
+
             case "praise":
                 text = random.choice(config.PRAISE_ANSWER)
+
             case "censure":
                 text = random.choice(config.CENSURE_ANSWER)
+
     elif cmd in config.VA_VOID_CMD_LIST:
         match cmd:
             case "current_time":
@@ -105,38 +124,48 @@ def execute_cmd(cmd: str) -> None:
                 )} {num2words.num2words(
                     now.minute, lang=config.LANGUAGE
                 )}"
+
             case "open_browser":
                 webbrowser.get(config.BASE_BROWSER).open_new_tab(config.BASE_URL)
                 text = random.choice(config.EXECUTE_ANSWER)
+
             case "open_youtube":
                 webbrowser.get(config.BASE_BROWSER).open_new_tab(config.YOUTUBE_URL)
                 text = random.choice(config.EXECUTE_ANSWER)
+
             case "run_goodbye_dpi":
                 os.system(config.GOODBYE_DPI_PATH)
                 text = random.choice(config.EXECUTE_ANSWER)
+
             case "sleep":
                 state_interface.sleep()
                 text = random.choice(config.EXECUTE_ANSWER)
+
             case "wake_up":
                 state_interface.wake_up(config.BASE_VOLUME)
                 text = random.choice(config.EXECUTE_ANSWER)
+
             case "turn_on_music":
                 pass
+
             case "turn_off_music":
                 pass
+
             case "poweroff":
                 a_s.va_speak(random.choice(config.POWEROFF_MESSAGE_LIST))
                 exit()
+
     print(f"Ответ от {config.VA_NAME}: {text}")
-    a_s.va_speak(text)
+    audio_speaking.va_speak(text)
 
 
-# response correction function
+# Response correction function
 def correct_response(response: str) -> str:
     bit_response = response.split()
     corrected_response = ''
     digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     flag = True
+
     for word in bit_response:
         for digit in digits:
             if digit in word:
@@ -145,31 +174,38 @@ def correct_response(response: str) -> str:
                 )} "
                 flag = False
                 break
+
         if flag: corrected_response += f"{word} "
         flag = True
+
     return corrected_response
 
 
-# function for receiving a response from ChatGPT
+# Function for receiving a response from ChatGPT
 def generate_response(dialogue_history: list[dict], message: str, mod: str, client=Client()):
     try:
-        if not dialogue_history:
+        if not dialogue_history: # Adding instruction for communication style
             dialogue_history.append(
                 {"role": "system", "content": config.PROMPT}
             )
+
         dialogue_history.append(
             {"role": "user", "content": message}
         )
+
         if mod == "base":
             chat_completion = client.chat.completions.create(
                 model=config.GPT_MODEL,
                 messages=dialogue_history
             )
             response = chat_completion.choices[0].message.content.strip()
+
             dialogue_history.append(
                 {"role": "assistant", "content": response}
             )
+
             corrected_response: str = correct_response(response)
+
             print(corrected_response)
             return corrected_response
         else:
@@ -180,20 +216,26 @@ def generate_response(dialogue_history: list[dict], message: str, mod: str, clie
                         messages=dialogue_history,
                     )
                     response = chat_completion.choices[0].message.content
+
                     dialogue_history.append(
                         {"role": "assistant", "content": response}
                     )
+
                     corrected_response: str = correct_response(response)
+
                     print(corrected_response)
                     return corrected_response
+
                 except Exception as err:
                     print(err)
                     continue
+
             return None
 
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         print(f"Message: {message}")
+
         return None
 
 
@@ -201,11 +243,15 @@ def generate_response(dialogue_history: list[dict], message: str, mod: str, clie
 def get_mod() -> str:
     mod: str = ''
     m = input(config.OPTIONS_MESSAGE)
+
     while m not in ('b', 'f'):
         m = input(config.OPTIONS_MESSAGE)
     else:
-        if m == 'b':
-            mod = "base"
-        elif m == 'f':
-            mod = "free"
+        match m:
+            case 'b':
+                mod = "base"
+
+            case 'f':
+                mod = "free"
+
     return mod
